a46372f1b9a08aa7991c7180641e676909de7169
hollow <clark09mitchell@gmail.com>
2026-02-12 12:53:34 -0500

    working on form
    Co-authored-by: 28cmarkham-dev <28cmarkham-dev@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 let cartObj = []
 // current things available for purcase: 
 // should make it so that the purchases menu is a form that goes to a email
+
+cartlink = document.getElementById("cart")
+
+cartlink.addEventListener("click", () => {
+    const destinationURL = 'cart.html';
+    window.location.href = destinationURL;
+})
