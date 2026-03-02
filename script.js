33e5232ca14bb374e545fd320d01a193e0b98085
hollow <clark09mitchell@gmail.com>
2026-03-02 08:29:41 -0500

    Co-authored-by: 28cmarkham-dev <28cmarkham-dev@users.noreply.github.com>

--- script.js
+++ script.js
@@ -9,3 +9,33 @@ cartlink.addEventListener("click", () => {
     const destinationURL = 'cart.html';
     window.location.href = destinationURL;
 })
+
+addtocart1 = document.getElementById("add_test1")
+
+addtocart1.addEventListener("click", () => {
+    cartObj.push("test1")})
+
+addtocart2 = document.getElementById("add_test2")
+
+addtocart2.addEventListener("click", () => {
+    cartObj.push("test2")})
+
+addtocart3 = document.getElementById("add_test3")
+
+addtocart3.addEventListener("click", () => {
+    cartObj.push("test3")})
+
+addtocart4 = document.getElementById("add_test4")
+
+addtocart4.addEventListener("click", () => {
+    cartObj.push("test4")})
+
+addtocart5 = document.getElementById("add_test5")
+
+addtocart5.addEventListener("click", () => {
+    cartObj.push("test5")})
+
+// pushtest = document.getElementById("pushtest")
+
+// pushtest.addEventListener("click", () => {
+//     console.log(cartObj)})
