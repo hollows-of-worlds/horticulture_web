906a3992b7c5bd0e1088ba982ab429a1d904d5bf
hollow <clark09mitchell@gmail.com>
2026-03-03 12:54:08 -0500

    worked on JS

--- script.js
+++ script.js
@@ -10,32 +10,36 @@ cartlink.addEventListener("click", () => {
     window.location.href = destinationURL;
 })
 
-addtocart1 = document.getElementById("day_lily")
+// addtocart1 = document.getElementById("day_lily")
 
-addtocart1.addEventListener("click", () => {
-    cartObj.push("Day lily")})
+// addtocart1.addEventListener("click", () => {
+//     cartObj.push("Day lily")})
 
-addtocart2 = document.getElementById("trellis_flower_white")
+// addtocart2 = document.getElementById("trellis_flower_white")
 
-addtocart2.addEventListener("click", () => {
-    cartObj.push("White trellis flower")})
+// addtocart2.addEventListener("click", () => {
+//     cartObj.push("White trellis flower")})
 
-addtocart3 = document.getElementById("sunflower")
+// addtocart3 = document.getElementById("sunflower")
 
-addtocart3.addEventListener("click", () => {
-    cartObj.push("Sunflower")})
+// addtocart3.addEventListener("click", () => {
+//     cartObj.push("Sunflower")})
 
-addtocart4 = document.getElementById("assortment_red_white")
+// addtocart4 = document.getElementById("assortment_red_white")
 
-addtocart4.addEventListener("click", () => {
-    cartObj.push("Red and white flower assortment")})
+// addtocart4.addEventListener("click", () => {
+//     cartObj.push("Red and white flower assortment")})
 
-addtocart5 = document.getElementById("assortment_pink_white")
+// addtocart5 = document.getElementById("assortment_pink_white")
 
-addtocart5.addEventListener("click", () => {
-    cartObj.push("Pink and white flower assortment")})
-
+// addtocart5.addEventListener("click", () => {
+//     cartObj.push("Pink and white flower assortment")})
+// console.log(cartObj)
 // pushtest = document.getElementById("pushtest")
 
 // pushtest.addEventListener("click", () => {
 //     console.log(cartObj)})
+
+function buyable5(){
+    console.log("clicked the dumbaass")
+}
